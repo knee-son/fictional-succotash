0517587704490d9e230c252f201244c45151b834
knee-son <83338313+knee-son@users.noreply.github.com>
2021-06-13 22:28:07 +0800

    Update webgl-demo.js

--- webgl-demo.js
+++ webgl-demo.js
@@ -242,7 +242,7 @@ function loadTexture(gl, url) {
   const border = 0;
   const srcFormat = gl.RGBA;
   const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  const pixel = new Uint8Array([255, 255, 255, 255]);  // opaque blue
   gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
                 width, height, border, srcFormat, srcType,
                 pixel);
@@ -281,7 +281,7 @@ function isPowerOf2(value) {
 // Draw the scene.
 //
 function drawScene(gl, programInfo, buffers, texture, deltaTime) {
-  gl.clearColor(245/255, 144/255, 226/255, 1.0);  // Clear to black, fully opaque
+  gl.clearColor(245/255, 171/255, 231/255, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing
   gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
